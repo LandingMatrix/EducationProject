86c7ed089cc0e6a4ba881f943826f60af59d2936
adaemaRue <cloney166@gmail.com>
2021-07-17 22:32:45 +1000

    a

--- src/Polls.js
+++ src/Polls.js
@@ -1,13 +1,14 @@
 import './Polls.css';
 
 const Polls = () => {
-    var votes = 60;
+    var votes = 10;
+    document.querySelector('div').style.setProperty('--width', votes+'%');
+    setTimeout(() => {  votes = 50; document.querySelector('div').style.setProperty('--width', votes+'%'); }, 2000);
 
-    
-    
     return (
-        <div class="bar"></div>
+        <div class='bar bar-transition'></div>
     );
+
 }
  
 export default Polls;
