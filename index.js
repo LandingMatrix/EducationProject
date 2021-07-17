10528543ddf4a682147a8088430855a118382979
CaptSzat <49853752+CaptSzat@users.noreply.github.com>
2021-07-17 12:38:50 +1000

    added inde.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+//to test the project just use
+//node index.js
+
+//then go to http://localhost:3000/
