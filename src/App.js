32a952912c93074e1b19d0cf35ed280a666c0f5d
adaemaRue <cloney166@gmail.com>
2021-07-18 04:33:33 +1000

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './Main.css';
 import NavBar from './NavBar';
-import Polls from './Polls';
+import Polls from './pollCmp/Polls';
 import Question from './Question';
 
 function App() {
@@ -11,7 +11,6 @@ function App() {
     <div className="App">
       <div className="content">
         <NavBar />
-        <h1>{ companyName }</h1>
         <Question />
         <Polls />
       </div>
