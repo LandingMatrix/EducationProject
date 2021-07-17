c724f149ff32345e6a3dd368daf0c112d8da0138
Kyndl3 <keanzhou2002@gmail.com>
2021-07-17 17:19:57 +1000

    twilio sid and token changes

--- src/twilioTest.js
+++ src/twilioTest.js
@@ -1,3 +1,9 @@
-var twilio = require('twilio');
+const twilio = require('twilio');
 
-var accSid = 
+//The SID and AuthToken are both test values right now - change to real values once the testing is finished
+const accSid = 'AC432db3e2731619e2170599d330c88f28';
+const authToken = '79a328ed4e7c6491203105c034f5ed1a';
+
+const clientSide = new twilio(process.env.accSid, process.env.authToken);
+
+let msg = [];
