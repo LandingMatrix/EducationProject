c0543b305debbbb77f5b5afc4a3d4a6f32ee99cf
CaptSzat <49853752+CaptSzat@users.noreply.github.com>
2021-07-17 21:50:52 +1000

    c

--- src/Question.js
+++ src/Question.js
@@ -6,7 +6,7 @@ function Question() {
   return (
     <div className="polling">
       <div className="question"><h1>Question: </h1> <input type="text"/></div>
-      <div className="multi"><h1>Multiple Choice: </h1> <h1>Yes</h1><input type="radio"/> <h1>No</h1> <input type="radio"/></div>
+      <div className="multi"><h1>Multiple Choice: </h1> <div className="connect"><h1>Yes</h1><input type="radio"/></div> <div className="connect"> <h1>No</h1><input type="radio"/></div></div>
     </div>
   );
 }
