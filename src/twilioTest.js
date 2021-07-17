66c3ff487bb0aafbe3f7abb854e2a5da749ca617
Kyndl3 <keanzhou2002@gmail.com>
2021-07-17 17:49:45 +1000

    phone num change

--- src/twilioTest.js
+++ src/twilioTest.js
@@ -4,13 +4,13 @@ const twilio = require('twilio');
 const accSid = 'AC687792804eaf16fea478a8fd0a9184b5';
 const authToken = '6956f63407e7da3f75e71d6f7458f360';
 
-const clientSide = new twilio(process.env.accSid, process.env.authToken);
+const clientSide = new twilio(accSid, authToken);
 
 let msg = [];
 
 clientSide.messages.create({
     body: 'Hello',
-    to: '+610434190715',
+    to: '+61434190715',
     from: '+61480093159'
 })
 .then(message => console.log(messages.sid))
