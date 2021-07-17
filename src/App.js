5cb7f39ae8411a40a528c0b1060f97e4f9aa745c
adaemaRue <cloney166@gmail.com>
2021-07-17 17:37:11 +1000

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Polls from '.Polls'
 
 function App() {
   const companyName = 'Edisco';
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <div className="content">
         <h1>{ companyName }</h1>
+        <Polls />
       </div>
     </div>
   );
---
+++ src/Polls.js
@@ -0,0 +1,9 @@
+const Polls = () => {
+    return (
+        <div className="Polls">
+            <p>test</p>
+        </div>
+      );
+}
+ 
+export default Polls;
