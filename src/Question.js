c2b4e109927ac1f46a7dfe4b86e11aabf9cb386f
CaptSzat <49853752+CaptSzat@users.noreply.github.com>
2021-07-18 08:45:44 +1000

    d

--- src/Question.js
+++ src/Question.js
@@ -1,13 +1,14 @@
 import logo from './logo.png';
 import profile from './profile.png';
 import './Main.css';
+import client from './client.js'
 
 function Question() {
   
   return (
     <div className="polling">
       <input type="text" placeholder="Propose a question..."/>
-      
+      <client.state.polls />
       {/* <div className="multi"><h1>Multiple Choice: </h1> <div className="connect"><h1>Yes</h1><input type="radio"/></div> <div className="connect"> <h1>No</h1><input type="radio"/></div></div> */}
     </div>
   );
