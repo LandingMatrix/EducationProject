6de286d1bbac4b0de24c1cec92229536b7223bc2
Kyndl3 <keanzhou2002@gmail.com>
2021-07-17 19:35:30 +1000

    test 2

---
+++ src/twilioTestTwo.js
@@ -0,0 +1,17 @@
+//require('dotenv').config()
+
+require('dotenv').config({ path: require('find-config')('.env') })
+
+//calling values from .env file - for security/privacy purposes
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken); 
+ 
+client.messages 
+      .create({ 
+         body: 'HIII :)))',  
+         messagingServiceSid: 'MG0d85bd1acab334cf22691b901a316e10',      
+         to: '+61434190715' 
+       }) 
+      .then(message => console.log(message.sid)) 
+      .done();
